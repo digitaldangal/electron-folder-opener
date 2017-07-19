9499187c5ce453e8462c561fdbad75c3653836b0
Raul6469 <vb4007@gmail.com>
2017-07-19 18:27:42 +0200

    Suppression code inutilisé

--- main.js
+++ main.js
@@ -22,31 +22,6 @@ if (process.defaultApp) {
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-/*
-function createWindow () {
-
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, show: false})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-*/
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -59,14 +34,6 @@ app.on('window-all-closed', function () {
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    //createWindow()
   }
 })
 
