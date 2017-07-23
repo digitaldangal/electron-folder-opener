012793373817c14741d24e7ef9f99f35b9cc29c5
Raul6469 <vb4007@gmail.com>
2017-07-23 12:27:55 +0200

    Open folder in renderer process

--- main.js
+++ main.js
@@ -12,21 +12,45 @@ const path = require('path')
 const url = require('url')
 
 const dialog = require('electron').dialog
-
-// # Here is code from this pull request : https://github.com/electron/electron-api-demos/pull/275
-// # This solves a problem where Windows was unable to launch the app from a web link
-if (process.defaultApp) {
-    // If we have the path to our app we set the protocol client to launch electron.exe with the path to our app
-    if (process.argv.length >= 2) {
-      app.setAsDefaultProtocolClient('folderopener', process.execPath, [path.resolve(process.argv[1])])
-    }
-    } else {
-      app.setAsDefaultProtocolClient('folderopener')
-}
+const ipc = require('electron').ipcMain
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// # Here is code from this pull request : https://github.com/electron/electron-api-demos/pull/275
+// # This solves a problem where Windows was unable to launch the app from a web link
+if (process.defaultApp) {
+  // If we have the path to our app we set the protocol client to launch electron.exe with the path to our app
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient('folderopener', process.execPath, [path.resolve(process.argv[1])])
+  }
+} else {
+  app.setAsDefaultProtocolClient('folderopener')
+}
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -37,29 +61,30 @@ app.on('window-all-closed', function () {
   }
 })
 
+let myPath;
+
 // # Here is what happens when the program is called by the protocol :
 app.makeSingleInstance(function (event, url) {
   var monUrl = event[2]; //Here is contained the URL that triggered the app
-  
+
   console.log(monUrl);
 
-  var myPath = decodeURI(monUrl.split('?path=')[1]); //We take the "parameter", which is our path to the folder we want to open
+  myPath = decodeURI(monUrl.split('?path=')[1]); //We take the "parameter", which is our path to the folder we want to open
 
-  // # Some lines to prepare the opening
-  const shell = require('electron').shell
-  const os = require('os')
+  createWindow();
 
-  console.log(myPath);
-
-  // # Finally, we open the folder
-  console.log(shell.showItemInFolder(myPath));
+  mainWindow.webContents.send(myPath);
 
   // # Once we're done, we close the app
-  app.quit();
+  //app.quit();
+})
+
+ipc.on('asynchronous-message', function (event, arg) {
+  event.sender.send('asynchronous-reply', myPath)
 })
 
 // # Since when we launch the app for the first time via the protocol it's unable to trigger the event above,
 // # we have to call the protocol twice from the web page.
 // # So we close the app that had not the chance to open the folder under 5 seconds.
 // # This prevents us from having a lot of Electron processes.
-setTimeout(function() { app.quit(); }, 5000);
+//setTimeout(function () { app.quit(); }, 5000);
--- renderer.js
+++ renderer.js
@@ -2,13 +2,18 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const shell = require('electron').shell
-
 const path = require('path')
+const os = require('os')
+const remote = require('electron').remote;
+const ipc = require('electron').ipcRenderer
 
 const protocolHandlerBtn = document.getElementById('protocol-handler')
 
-protocolHandlerBtn.addEventListener('click', function () {
-  const pageDirectory = __dirname.replace('app.asar', 'app.asar.unpacked')
-  const pagePath = path.join('file://', pageDirectory, '../../sections/system/protocol-link.html')
-  shell.openExternal(pagePath)
+ipc.on('asynchronous-reply', function (event, arg) {
+    var myPath = arg;
+    var reussite = shell.showItemInFolder(myPath);
+    var window = remote.getCurrentWindow();
+    window.close();
 })
+
+ipc.send('asynchronous-message', 'ping')
