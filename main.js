57ed77bf11c8aab69e03ccd173a5b5f0b98b7d2a
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-09-04 19:13:17 +0200

    Fix code issues in main.js

--- main.js
+++ main.js
@@ -2,20 +2,20 @@
 * Lines starting with a # are written by me.
 */
 
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
-const dialog = require('electron').dialog
-const ipc = require('electron').ipcMain
+const path = require('path');
+const url = require('url');
+const dialog = require('electron').dialog;
+const ipc = require('electron').ipcMain;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow() {
   // Create the browser window.
@@ -26,7 +26,7 @@ function createWindow() {
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true,
-  }))
+  }));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -37,18 +37,18 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
-  })
-}
+  });
+};
 
 // # Here is code from this pull request : https://github.com/electron/electron-api-demos/pull/275
 // # This solves a problem where Windows was unable to launch the app from a web link
 if (process.defaultApp) {
   // If we have the path to our app we set the protocol client to launch electron.exe with the path to our app
   if (process.argv.length >= 2) {
-    app.setAsDefaultProtocolClient('folderopener', process.execPath, [path.resolve(process.argv[1])])
+    app.setAsDefaultProtocolClient('folderopener', process.execPath, [path.resolve(process.argv[1])]);
   }
 } else {
-  app.setAsDefaultProtocolClient('folderopener')
+  app.setAsDefaultProtocolClient('folderopener');
 }
 
 // Quit when all windows are closed.
@@ -56,11 +56,11 @@ app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    console.log("Quit");
+    console.log('Quit');
   }
-})
+});
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 let myPath;
 
@@ -72,18 +72,18 @@ var shouldquit = app.makeSingleInstance(function (event, url) {
 
   myPath = decodeURI(myUrl.split('?path=')[1]); //We take the "parameter", which is our path to the folder we want to open
 
-  mainWindow.webContents.send('info', myPath)
+  mainWindow.webContents.send('info', myPath);
 
   console.log('Msg sent to renderer : ' + myPath);
 
 })
 
 if (shouldquit) {
-  app.quit()
+  app.quit();
 }
 
 ipc.on('asynchronous-message', function (event, arg) {
-  if (arg == "ready") {
-    event.sender.send('asynchronous-reply', myPath)
+  if (arg == 'ready') {
+    event.sender.send('asynchronous-reply', myPath);
   }
 })
