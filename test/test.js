cf8663d3c68778b4f73fa5ef673f60e62d2bdc37
Victor <vb4007@gmail.com>
2018-01-27 13:01:08 +0100

    Very important typo

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 let chai = require('chai');
 let should = chai.should();
 
-describe('Am I to lazy to write unit tests ?', function() {
+describe('Am I too lazy to write unit tests ?', function() {
     it('Yes', (done) => {
         "Absolutely".should.equals("Absolutely")
         done()
